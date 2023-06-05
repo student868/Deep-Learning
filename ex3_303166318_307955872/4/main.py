041e979d7f6c8583dc33ff7323cebfddca9f5f92
Ofir <@>
2023-06-05 12:32:26 +0300

    hw3 q4 DC-GAN works

--- ex3_303166318_307955872/4/main.py
+++ ex3_303166318_307955872/4/main.py
@@ -1,23 +1,19 @@
 import os.path
 
-import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import torchvision.utils
-from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
 from models import GeneratorWGAN, GeneratorDCGAN, DiscriminatorWGAN, DiscriminatorDCGAN
-from trainer import g_train_batch, d_train_batch
+from trainer import train_models, plot_samples, plot_one_sample
 
 BATCH_SIZE = 64
-PRINT_EVERY = 200
 EPOCHS = 200
 
 DIM = 64
 Z_SIZE = 128
-DISCRIMINATOR_ITERATIONS = 5  # For WGAN, number of discriminator iterations per generator iterations
 
 MODELS_DIR = 'models'
 
@@ -28,11 +24,6 @@ def load_data(dataset):
     training_data = dataset(root="data", train=True, download=True, transform=ToTensor())
     dataloader = DataLoader(training_data, batch_size=BATCH_SIZE, shuffle=True)
     return dataloader
-
-
-def init_weights(layer):
-    if isinstance(layer, nn.Linear):
-        nn.init.xavier_normal_(layer.weight)
 
 
 def plot_loss(lost_list1, lost_list2=None, labels=('W-GAN', 'DC-GAN')):
@@ -53,22 +44,6 @@ def plot_loss(lost_list1, lost_list2=None, labels=('W-GAN', 'DC-GAN')):
     plt.show()
 
 
-def plot_samples(g, title, n_samples=100, n_rows=10):
-    plt.title(title)
-    noise = torch.randn((n_samples, g.z_size)).to(device)
-    img = g(noise).reshape((-1, 1, 28, 28))
-
-    grid = torchvision.utils.make_grid(img, nrow=n_rows).cpu().detach().numpy()
-    grid = np.transpose(grid, (1, 2, 0))
-    plt.imshow(grid, cmap='gray')
-    plt.axis('off')
-    plt.show()
-
-
-def plot_one_sample(g, title):
-    plot_samples(g, title, n_samples=1, n_rows=1)
-
-
 def load_models(g, d, g_save_path, d_save_path):
     print('Loading old weights from "' + g_save_path + '" and "' + d_save_path + '"')
     g.load_state_dict(torch.load(g_save_path, map_location=torch.device(device)))
@@ -76,62 +51,17 @@ def load_models(g, d, g_save_path, d_save_path):
     print("Loaded models!")
 
 
-def train_models(dataloader, g, d, epochs, g_save_path, d_save_path, save=False):
-    print('Training...')
-
-    g_loss_list = []
-    d_loss_list = []
-
-    if isinstance(g, GeneratorWGAN) and isinstance(d, DiscriminatorWGAN):
-        g_optimizer = torch.optim.RMSprop(g.parameters(), lr=5e-5)
-        d_optimizer = torch.optim.RMSprop(d.parameters(), lr=5e-5)
-    elif isinstance(g, GeneratorDCGAN) and isinstance(d, DiscriminatorDCGAN):
-        betas = (0.5, 0.9)
-        g_optimizer = torch.optim.Adam(g.parameters(), lr=1e-4, betas=betas)
-        d_optimizer = torch.optim.Adam(d.parameters(), lr=1e-4, betas=betas)
-    else:
-        raise ValueError("Generator / Discriminator Error!")
-
-    for epoch in range(epochs):
-        for iteration, (X, _) in enumerate(dataloader):
-            d_loss_list.append(d_train_batch(device, dataloader, g, d, d_optimizer, X))
-
-            if isinstance(g, GeneratorWGAN) and isinstance(d, DiscriminatorWGAN):  # use DISCRIMINATOR_ITERATIONS for WGAN
-                if iteration > 0 and iteration % DISCRIMINATOR_ITERATIONS == 0:
-                    g_loss_list.append(g_train_batch(device, dataloader, g, d, g_optimizer))
-            else:  # train generator after every iteration of the discriminator
-                g_loss_list.append(g_train_batch(device, dataloader, g, d, g_optimizer))
-
-            if (iteration + 1) % PRINT_EVERY == 0:
-                print("Epoch [{:>4}/{:>4}] Iteration [{:>4}/{:>4}] - Training Loss: (G: {:>+10.5f}, D: {:>+10.5f})".format(
-                    epoch + 1, epochs, iteration + 1, len(dataloader), g_loss_list[-1], d_loss_list[-1]))
-
-        plot_samples(g, g.mode + ' Samples - Epoch #' + str(epoch + 1))
-
-    # Save the models
-    if save:
-        print('Saving models to "' + g_save_path + '" and "' + d_save_path + '"')
-        torch.save(g.state_dict(), g_save_path)
-        torch.save(d.state_dict(), d_save_path)
-
-    return g_loss_list, d_loss_list
-
-
 def update_models(dataloader, g, d, epochs, use_saved_weights):
     g_save_path = os.path.join(MODELS_DIR, g.name + '.pkl')
     d_save_path = os.path.join(MODELS_DIR, d.name + '.pkl')
-
-    g.apply(init_weights)
-    d.apply(init_weights)
 
     d_loss_list = None
 
     if use_saved_weights and (os.path.exists(g_save_path) and os.path.exists(d_save_path)):
         load_models(g, d, g_save_path, d_save_path)
     else:
-        g_loss_list, d_loss_list = train_models(dataloader, g, d, epochs, g_save_path, d_save_path)
+        g_loss_list, d_loss_list = train_models(device, dataloader, g, d, epochs, g_save_path, d_save_path, save=True)  # TODO remove save
 
-    plot_samples(g, 'Samples')
     return d_loss_list
 
 
@@ -140,14 +70,19 @@ def main():
 
     g = GeneratorWGAN(DIM, Z_SIZE).to(device)
     d = DiscriminatorWGAN(DIM).to(device)
-    wgan_d_loss = update_models(dataloader, g, d, epochs=EPOCHS, use_saved_weights=False)
+    wgan_d_loss = update_models(dataloader, g, d, EPOCHS, use_saved_weights=False)
+    plot_samples(device, g, 'W-GAN')
+    plot_one_sample(device, g, 'W-GAN')
 
+    exit()
     print('#' * 50)
     print()
 
     g = GeneratorDCGAN(DIM, Z_SIZE).to(device)
     d = DiscriminatorDCGAN(DIM).to(device)
-    dcgan_d_loss = update_models(dataloader, g, d, epochs=EPOCHS, use_saved_weights=False)
+    dcgan_d_loss = update_models(dataloader, g, d, EPOCHS, use_saved_weights=False)
+    plot_samples(device, g, 'DC-GAN')
+    plot_one_sample(device, g, 'DC-GAN')
 
     plot_loss(wgan_d_loss, dcgan_d_loss)
 
--- ex3_303166318_307955872/4/trainer.py
+++ ex3_303166318_307955872/4/trainer.py
@@ -1,7 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+import torchvision.utils
+from torch import nn
+
 from models import GeneratorWGAN, DiscriminatorWGAN, GeneratorDCGAN, DiscriminatorDCGAN
 
 DISCRIMINATOR_WEIGHT_CLIP = 0.01
+DISCRIMINATOR_ITERATIONS = 5  # For WGAN, number of discriminator iterations per generator iterations
+
+PRINT_EVERY = 500
 
 
 def wgan_g_loss_fn(d_score_on_fake):
@@ -12,12 +20,14 @@ def wgan_d_loss_fn(d_score_on_fake, d_score_on_real):
     return -1 * (torch.mean(d_score_on_real) - torch.mean(d_score_on_fake))  # -1 to make maximization problem to minimization problem
 
 
-# def dcgan_g_loss_fn(d_score_on_fake):
-#     return torch.mean(torch.log(1 - d_score_on_fake))
-#
-#
-# def dcgan_d_loss_fn(d_score_on_fake, d_score_on_real):
-#     return -1 * (torch.mean(torch.log(d_score_on_real)) + torch.mean(torch.log(1 - d_score_on_fake)))  # -1 to make maximization problem to minimization problem
+def dcgan_g_loss_fn(d_score_on_fake):
+    return nn.BCEWithLogitsLoss()(d_score_on_fake, torch.full_like(d_score_on_fake, 1))
+
+
+def dcgan_d_loss_fn(d_score_on_fake, d_score_on_real):
+    fake_loss = nn.BCEWithLogitsLoss()(d_score_on_fake, torch.full_like(d_score_on_fake, 0))  # 0 for fake
+    real_loss = nn.BCEWithLogitsLoss()(d_score_on_real, torch.full_like(d_score_on_real, 1))  # 1 for real
+    return (fake_loss + real_loss) / 2
 
 
 def g_train_batch(device, dataloader, g, d, optimizer):
@@ -41,12 +51,29 @@ def g_train_batch(device, dataloader, g, d, optimizer):
     return loss.item()
 
 
+def plot_samples(device, g, title, n_samples=25, n_rows=5):
+    g.eval()
+    plt.title(title)
+    noise = torch.randn((n_samples, g.z_size)).to(device)
+    img = g(noise).reshape((-1, 1, 28, 28))
+
+    grid = torchvision.utils.make_grid(img, nrow=n_rows).cpu().detach().numpy()
+    grid = np.transpose(grid, (1, 2, 0))
+    plt.imshow(grid, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+
+def plot_one_sample(device, g, title):
+    plot_samples(device, g, title, n_samples=1, n_rows=1)
+
+
 def d_train_batch(device, dataloader, g, d, optimizer, X):
     d.train()
     g.eval()
 
     noise = torch.randn((dataloader.batch_size, g.z_size)).to(device)
-    score_on_fake = d(g(noise))
+    score_on_fake = d(g(noise).detach())
 
     X = X.to(device)
     score_on_real = d(X)
@@ -69,3 +96,44 @@ def d_train_batch(device, dataloader, g, d, optimizer, X):
                 param.clamp_(-DISCRIMINATOR_WEIGHT_CLIP, DISCRIMINATOR_WEIGHT_CLIP)
 
     return loss.item()
+
+
+def train_models(device, dataloader, g, d, epochs, g_save_path, d_save_path, save=False):
+    print('Training...')
+
+    g_loss_list = []
+    d_loss_list = []
+
+    if isinstance(g, GeneratorWGAN) and isinstance(d, DiscriminatorWGAN):
+        g_optimizer = torch.optim.RMSprop(g.parameters(), lr=5e-5)
+        d_optimizer = torch.optim.RMSprop(d.parameters(), lr=5e-5)
+    elif isinstance(g, GeneratorDCGAN) and isinstance(d, DiscriminatorDCGAN):
+        betas = (0.5, 0.999)
+        g_optimizer = torch.optim.Adam(g.parameters(), lr=2e-4, betas=betas)
+        d_optimizer = torch.optim.Adam(d.parameters(), lr=2e-4, betas=betas)
+    else:
+        raise ValueError("Generator / Discriminator Error!")
+
+    for epoch in range(epochs):
+        for iteration, (X, _) in enumerate(dataloader):
+            d_loss_list.append(d_train_batch(device, dataloader, g, d, d_optimizer, X))
+
+            if isinstance(g, GeneratorWGAN) and isinstance(d, DiscriminatorWGAN):  # use DISCRIMINATOR_ITERATIONS for WGAN
+                if iteration > 0 and iteration % DISCRIMINATOR_ITERATIONS == 0:
+                    g_loss_list.append(g_train_batch(device, dataloader, g, d, g_optimizer))
+            else:  # train generator after every iteration of the discriminator
+                g_loss_list.append(g_train_batch(device, dataloader, g, d, g_optimizer))
+
+            if ((iteration + 1) % PRINT_EVERY == 0 or iteration + 1 == len(dataloader)) and (len(g_loss_list) > 0) and (len(d_loss_list) > 0):
+                print("Epoch [{:>4}/{:>4}] Iteration [{:>4}/{:>4}] - Training Loss: (G: {:>+10.5f}, D: {:>+10.5f})".format(
+                    epoch + 1, epochs, iteration + 1, len(dataloader), g_loss_list[-1], d_loss_list[-1]))  # fix g_loss_list is empty for printevery = 1
+
+        plot_samples(device, g, g.mode + ' Samples - Epoch #' + str(epoch + 1))
+
+    # Save the models
+    if save:
+        print('Saving models to "' + g_save_path + '" and "' + d_save_path + '"')
+        torch.save(g.state_dict(), g_save_path)
+        torch.save(d.state_dict(), d_save_path)
+
+    return g_loss_list, d_loss_list
