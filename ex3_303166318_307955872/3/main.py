459987881605a478782eae3947e49293dc560d52
Ofir <@>
2023-06-13 11:24:34 +0300

    hw3 major revision

--- ex3_303166318_307955872/3/main.py
+++ ex3_303166318_307955872/3/main.py
@@ -1,4 +1,6 @@
 import os.path
+
+import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -29,120 +31,132 @@ def load_data(dataset):
 
 def init_weights(layer):
     if isinstance(layer, nn.Linear):
-        nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')  # best initialization even though activation is softplus
+        nn.init.kaiming_normal_(layer.weight, nonlinearity='relu')  # best initialization even though activation is soft-plus
 
 
-def construct_svm_data(train_dataloader, test_dataloader, labeled_samples, model):
+def svm_train_data(train_dataset, labeled_samples, model_nn):
+    train_dataloader = DataLoader(train_dataset, batch_size=len(train_dataset))
+
     # Get balanced classes
-    X_train, y_train = next(iter(train_dataloader))
-    X_train = X_train.flatten(1)
+    x_train, y_train = next(iter(train_dataloader))
+    x_train = x_train.flatten(1)
     num_classes = len(y_train.unique())
     indices_of_balanced_classes = torch.cat([(y_train == i).nonzero()[:(labeled_samples // num_classes)].flatten() for i in range(num_classes)])
-    X_train = X_train[indices_of_balanced_classes].to(device)
+    x_train = x_train[indices_of_balanced_classes].to(device)
     y_train = y_train[indices_of_balanced_classes]
 
     # Get latent representation
-    mu, sigma, _ = model(X_train)
-    X_train = torch.cat([mu, sigma], dim=1)
+    mu, sigma, _ = model_nn(x_train)
+    x_train = torch.cat([mu, sigma], dim=1)
+    x_train = x_train.cpu()
 
-    X_test, y_test = next(iter(test_dataloader))
-    X_test = X_test.to(device)
-    mu, sigma, _ = model(X_test.flatten(1))
-    X_test = torch.cat([mu, sigma], dim=1)
-
-    X_train = X_train.cpu()
-    X_test = X_test.cpu()
-
-    return (X_train.detach().numpy(), y_train.detach().numpy()), \
-           (X_test.detach().numpy(), y_test.detach().numpy())
+    return x_train.detach().numpy(), y_train.detach().numpy()
 
 
-def train(train_dataset, test_dataset, labeled_samples, use_saved_weights):
-    # Train VAE
-    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
-    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+def svm_test_data(test_dataset, model_nn):
+    test_dataloader = DataLoader(test_dataset, batch_size=len(test_dataset))
 
-    model = Vae(
+    # Get latent representation
+    x_test, y_test = next(iter(test_dataloader))
+    x_test = x_test.to(device)
+    mu, sigma, _ = model_nn(x_test.flatten(1))
+    x_test = torch.cat([mu, sigma], dim=1)
+    x_test = x_test.cpu()
+
+    return x_test.detach().numpy(), y_test.detach().numpy()
+
+
+def train_nn(train_dataloader, test_dataloader, model, construction_loss_fn):
+    model.apply(init_weights)
+
+    print('Training NN model...')
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=10)
+
+    epoch = 0
+    while optimizer.param_groups[0]['lr'] > TRAIN_LR_STOP:
+        train_epoch(device, train_dataloader, model, construction_loss_fn, optimizer)
+        train_loss = test_epoch(device, train_dataloader, model, construction_loss_fn)
+        test_loss = test_epoch(device, test_dataloader, model, construction_loss_fn)
+        if epoch % PRINT_EVERY == 0:
+            print("Epoch {}, LR: {:8.6f}, Train Loss: {:>0.3f}, Test Loss: {:>0.3f}".format(
+                epoch + 1, optimizer.param_groups[0]['lr'], train_loss, test_loss))
+        scheduler.step(test_loss)
+        epoch += 1
+
+
+def update_nn(train_dataset, test_dataset, use_saved_weights, save=False):
+    model_nn = Vae(
         train_dataset[0][0].flatten().size(0),
         HIDDEN_SIZE,
         Z_SIZE,
         train_dataset.__class__.__name__ + '_NN'
     ).to(device)
-    model.apply(init_weights)
-    nn_save_path = os.path.join(MODELS_DIR, model.name + '.pkl')
 
+    save_path = os.path.join(MODELS_DIR, model_nn.name + '.pkl')
     construction_loss_fn = nn.BCELoss(reduction='sum')
 
-    if use_saved_weights and os.path.exists(nn_save_path):
-        # Load the NN model
-        print('Loading NN old weights from "' + nn_save_path + '"')
-        model.load_state_dict(torch.load(nn_save_path, map_location=torch.device(device)))
-        test_loss = test_epoch(device, test_dataloader, model, construction_loss_fn)
-        print("Loaded NN model - Test Loss: {:>0.3f}".format(test_loss))
+    train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+    test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
+    if use_saved_weights and os.path.exists(save_path):
+        print('Loading NN weights from "' + save_path + '"')
+        model_nn.load_state_dict(torch.load(save_path, map_location=torch.device(device)))
     else:
-        print('Training NN model...')
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=10)
-        epoch = 0
-        while optimizer.param_groups[0]['lr'] > TRAIN_LR_STOP:
-            train_epoch(device, train_dataloader, model, construction_loss_fn, optimizer)
-            train_loss = test_epoch(device, train_dataloader, model, construction_loss_fn)
-            test_loss = test_epoch(device, test_dataloader, model, construction_loss_fn)
-            if epoch % PRINT_EVERY == 0:
-                print("Epoch {}, LR: {:8.6f}, Train Loss: {:>0.3f}, Test Loss: {:>0.3f}".format(
-                    epoch + 1, optimizer.param_groups[0]['lr'], train_loss, test_loss))
-            scheduler.step(test_loss)
-            epoch += 1
+        train_nn(train_dataloader, test_dataloader, model_nn, construction_loss_fn)
+        if save:
+            print('Saving NN model to "' + save_path + '"')
+            torch.save(model_nn.state_dict(), save_path)
 
-        train_loss = test_epoch(device, train_dataloader, model, construction_loss_fn)
-        test_loss = test_epoch(device, test_dataloader, model, construction_loss_fn)
-        print("Epoch {}, LR: {:8.6f}, Train Loss: {:>0.3f}, Test Loss: {:>0.3f}".format(
-            epoch + 1, optimizer.param_groups[0]['lr'], train_loss, test_loss))
+    test_loss = test_epoch(device, test_dataloader, model_nn, construction_loss_fn)
+    print("NN Test Loss: {:>0.3f}".format(test_loss))
+    print()
 
-        # Save the NN model
-        print('Saving NN model to "' + nn_save_path + '"')
-        # torch.save(model.state_dict(), nn_save_path)
+    return model_nn
 
-    # Train SVM
-    svm_save_path = os.path.join(MODELS_DIR, train_dataset.__class__.__name__ + '_SVM' + '_' + str(labeled_samples) + '.pkl')
-    (X_train, y_train), (X_test, y_test) = construct_svm_data(
-        DataLoader(train_dataset, batch_size=len(train_dataset)),
-        DataLoader(test_dataset, batch_size=len(test_dataset)),
-        labeled_samples,
-        model)
-    svm_classifier = svm.SVC(kernel='poly', degree=5)
 
-    if use_saved_weights and os.path.exists(svm_save_path):
-        # Load the SVM model
-        print('Loading SVM old weights from "' + svm_save_path + '"')
-        svm_classifier = pickle.load(open(svm_save_path, 'rb'))
+def update_svm(train_dataset, labeled_samples, model_nn, use_saved_weights, save=False):
+    save_path = os.path.join(MODELS_DIR, train_dataset.__class__.__name__ + '_SVM' + '_' + str(labeled_samples) + '.pkl')
 
+    if use_saved_weights and os.path.exists(save_path):
+        print('Loading SVM weights from "' + save_path + '"')
+        svm_classifier = pickle.load(open(save_path, 'rb'))
     else:
         print('Training SVM model...')
-        svm_classifier.fit(X_train, y_train)
+        svm_classifier = svm.SVC(kernel='poly', degree=5)
+        x_train, y_train = svm_train_data(train_dataset, labeled_samples, model_nn)
+        svm_classifier.fit(x_train, y_train)
+        if save:
+            print('Saving SVM model to "' + save_path + '"')
+            pickle.dump(svm_classifier, open(save_path, 'wb'))
 
-        # Save the SVM model
-        print('Saving SVM model to "' + svm_save_path + '"')
-        pickle.dump(svm_classifier, open(svm_save_path, 'wb'))
+    return svm_classifier
 
-    pred = svm_classifier.predict(X_test)
-    print('Accuracy Error for {} labeled samples: {:>0.2f}%'.format(labeled_samples, 100 * (1 - (pred == y_test).mean())))
+
+def evaluate(test_dataset, model_nn, model_svm):
+    x_test, y_test = svm_test_data(test_dataset, model_nn)
+    pred: np.ndarray = model_svm.predict(x_test)
+    accuracy = 100 * (1 - (pred == y_test).mean())
+    print('Accuracy Error: {:>0.2f}%'.format(accuracy))
     print()
 
 
-def main():
-    training_data, test_data = load_data(datasets.MNIST)
+def main(use_saved_weights=True):
+    train_dataset, test_dataset = load_data(datasets.MNIST)
+    model_nn = update_nn(train_dataset, test_dataset, use_saved_weights=use_saved_weights)
     for labeled_samples in [100, 600, 1000, 3000]:
-        train(training_data, test_data, labeled_samples, use_saved_weights=True)
+        model_svm = update_svm(train_dataset, labeled_samples, model_nn, use_saved_weights=use_saved_weights)
+        evaluate(test_dataset, model_nn, model_svm)
 
     print('#' * 50)
     print()
 
-    training_data, test_data = load_data(datasets.FashionMNIST)
+    train_dataset, test_dataset = load_data(datasets.FashionMNIST)
+    model_nn = update_nn(train_dataset, test_dataset, use_saved_weights=use_saved_weights)
     for labeled_samples in [100, 600, 1000, 3000]:
-        train(training_data, test_data, labeled_samples, use_saved_weights=True)
+        model_svm = update_svm(train_dataset, labeled_samples, model_nn, use_saved_weights=use_saved_weights)
+        evaluate(test_dataset, model_nn, model_svm)
 
 
 if __name__ == '__main__':
-    main()
+    main(False)
--- ex3_303166318_307955872/3/trainer.py
+++ ex3_303166318_307955872/3/trainer.py
@@ -1,15 +1,21 @@
 import torch
 
 
+def compute_loss(device, x, model, construction_loss_fn):
+    # Forward
+    x = x.to(device).flatten(1)
+    mu, sigma, pred = model(x)
+
+    # Compute loss
+    kl_div = - torch.sum(1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2))
+    loss = construction_loss_fn(pred, x) + kl_div
+    return loss
+
+
 def train_epoch(device, dataloader, model, construction_loss_fn, optimizer):
     model.train()
-    for batch, (X, _) in enumerate(dataloader):
-        X = X.to(device).flatten(1)
-        mu, sigma, pred = model(X)
-
-        # Compute loss
-        kl_div = - torch.sum(1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2))
-        loss = construction_loss_fn(pred, X) + kl_div
+    for batch, (x, _) in enumerate(dataloader):
+        loss = compute_loss(device, x, model, construction_loss_fn)
 
         # Backpropagation
         optimizer.zero_grad()
@@ -21,12 +27,7 @@ def test_epoch(device, dataloader, model, construction_loss_fn):
     model.eval()
     loss = 0
     with torch.no_grad():
-        for X, _ in dataloader:
-            X = X.to(device).flatten(1)
-            mu, sigma, pred = model(X)
-
-            # Compute loss
-            kl_div = - torch.sum(1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2))
-            loss += construction_loss_fn(pred, X) + kl_div
+        for x, _ in dataloader:
+            loss += compute_loss(device, x, model, construction_loss_fn)
     loss /= len(dataloader.dataset)
     return loss
